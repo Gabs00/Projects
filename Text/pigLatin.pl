3effaec8bdb607f634c4f2a6dbd45f81a688b377
Gabs00 <gewen87@gmail.com>
2014-02-06 16:57:15 -0500

    Create pigLatin.pl

---
+++ Text/pigLatin.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+
+my $sentence;
+my $go = 1;
+print "\n", pigSentence("Welcome to the pig latin converter."), "\n\n";
+
+while($go){
+	while(!($sentence)){
+		print "Please type a sentence to be translated into igpay atinlay: ";
+		chomp ($sentence = <>);
+	}
+
+	print "\n", pigSentence($sentence), "\n";
+	
+	print "Another? (y/n):";
+	chomp (my $ans = <>);
+	
+	$go = ($ans eq 'n') ? 0:1;
+}
+
+
+sub pigSentence {
+	my $sentence = $_[0];
+	chomp $sentence;
+	my $punc = qr/\.|\?|\,|\!/;
+	
+	my @words = split ' ', $sentence;
+	for my $i(0..$#words){
+		my $per;
+		if($words[$i] =~ $punc) { $per = chop $words[$i]; }
+		
+		$words[$i] = pigWord($words[$i]);
+		
+		if($per){
+			$words[$i].= $per;
+		}
+		
+	}
+
+	return join(' ', @words);
+}
+
+sub pigWord {
+	my @word = split '', $_[0];
+	my $vowels = qr/a|e|i|o|u |\d/;
+	if($word[0] !~ $vowels) { 
+		@word = @{ popShift(@word) };
+		if($word[0] !~ $vowels){
+			@word = @{ popShift(@word) };
+		}
+	}
+	else {
+		push @word, "w";
+	}
+	push @word, "ay";
+	
+	return join('', @word);
+
+}
+
+sub popShift {
+	my @array = @_;
+	my $first = shift @array;
+	push @array, $first;
+	return\@array;
+}

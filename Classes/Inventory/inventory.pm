3e88c9e026a977e3f7b037c7117847d14bb95dc4
Gabs00 <gewen87@gmail.com>
2014-02-17 01:43:55 -0500

    Create inventory.pm
    This is an incomplete class

---
+++ Classes/Inventory/inventory.pm
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+package Inventory;
+use Moose;
+use namespace::autoclean;
+
+has productList => (
+	is => 'rw',
+	isa => 'HashRef',
+	writer => '_set_productList',
+	default => sub {{}},
+);
+
+has items => (
+		is => 'rw',
+		isa => 'Int',
+		writer => '_set_items',
+		builder => 'item_list',
+		lazy => 1,
+);
+
+
+
+1;
